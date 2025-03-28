0f99ea365cdeaf66b5e14a28cacdf1748872141d
QuerenHapuque52 <querenjesussilva.52@hotmail.com>
2025-03-28 12:33:50 -0300

    Update Dart28mar2025.dart
    Nesta aula, exploramos o uso de laços `for` e listas em Dart, através de uma série de exercícios práticos. Através destes exercícios, aprendemos a:
    
    1. Exercício 1 (`ex28mar1`): Utilizar um laço `for` para imprimir números de 1 a 10, reforçando a estrutura básica de um loop.
    
    2. Exercício 2 (`ex28mar2`): Imprimir números pares de 2 a 20, demonstrando como modificar o passo do loop para atender a condições específicas.
    
    3. Exercício 3 (`ex28mar3`): Calcular e imprimir a soma dos números de 1 a 100, ilustrando o uso de variáveis acumuladoras dentro de um laço.
    
    4. Exercício 4 (`ex28mar4`): Criar e manipular listas em Dart, adicionando elementos e acessando valores específicos através de seus índices.
    
    5. Exercício 5 (`ex28mar5`): Repetir a impressão dos nomes armazenados em uma lista, utilizando um laço `for` para acessar cada elemento individualmente.
    
    6. Exercício 6 (`ex28mar6`): Calcular e imprimir a média dos valores em uma lista, mostrando como somar elementos e dividir pelo total para obter uma média.
    
    Esses exercícios proporcionaram uma compreensão prática das estruturas de repetição e manipulação de listas, fundamentais na programação em Dart. Ao final da aula, os alunos se sentiram mais confiantes em usar loops e listas para resolver problemas comuns na programação.

--- Dart28mar2025.dart
+++ Dart28mar2025.dart
@@ -1,23 +1,13 @@
 void main() {
-  ex28mar4();
+  ex28mar1();
 }
 
 //for(valor inicial de i, condição parada;passo).
   ex28mar1(){
-  for(int i=1;i<=10;i=10){
+  for(int i=1; i<=10; i++){
     print("Exec. n.:$i");
   }
 }  
-  ex28mar6(){
-    //dado a lista abaixo, imprima a média dos valores
-    List<int> valores = [1, 6, 9, 3, 3, 9, 9, 3, 7];
-    int soma = 0;
-    for (var i = 0; i < valores.length; i++){
-      soma = soma + valores[i];
-    }
-    print(soma/valores.length);
-  }
-
 
   ex28mar2(){
     //imprimir 2 4 6...20
@@ -45,4 +35,26 @@ void main() {
     alunos.add("Aline");
     print(alunos);
     print(alunos[0]);
+  }
+
+  ex28mar5(){
+    List alunos = []; //cria uma lista vazia
+    alunos.add("Ana");
+    alunos.add("João");
+    alunos.add("Carlos");
+    alunos.add("Aline");
+    //imprima um aluno por linha usando o for
+    for(int i = 0; i < alunos.length; i++){
+      print(alunos[i]);
+    }
+  }
+
+  ex28mar6(){
+    //dado a lista abaixo, imprima a média dos valores
+    List<int> valores = [1, 6, 9, 3, 3, 9, 9, 3, 7];
+    int soma = 0;
+    for (var i = 0; i < valores.length; i++){
+      soma = soma + valores[i];
+    }
+    print(soma/valores.length);
   }
